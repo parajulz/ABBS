6d26c06ccf49127e4597b03071bba3b9906db5d7
ceciliasanjaya <cecs@berkeley.edu>
2025-04-24 23:07:29 -0700

    initial

---
+++ ABBSapp/ABBSapp/ABBSappApp.swift
@@ -0,0 +1,17 @@
+//
+//  ABBSappApp.swift
+//  ABBSapp
+//
+//  Created by Cecilia on 4/24/25.
+//
+
+import SwiftUI
+
+@main
+struct ABBSappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ABBSapp/ABBSappTests/ABBSappTests.swift
@@ -0,0 +1,17 @@
+//
+//  ABBSappTests.swift
+//  ABBSappTests
+//
+//  Created by Cecilia on 4/24/25.
+//
+
+import Testing
+@testable import ABBSapp
+
+struct ABBSappTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
